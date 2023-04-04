460b175228dd7e06a153a1f598105e7ba7704dab
Захаров Артем <zaharov-art@mail.ru>
2023-04-04 20:49:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+struct DateTime {
+	int year;
+	int month;
+	int day;
+};
+struct listofwork {
+	char name = { 50 };
+	char prioritets = { 200 };
+	char description = { 350 };
+	DateTime projektdevilery = { 00,00,00 };
+};
+int main()
+{
+
+	
+
+}
